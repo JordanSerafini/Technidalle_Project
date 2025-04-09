e4fe5b11b3d457a97dfae726d130358ff069a6d9
JordanS <jordanserafini.74@gmail.com>
2025-04-09 16:45:03 +0200

    working

--- Backend/api_gateway/src/main.ts
+++ Backend/api_gateway/src/main.ts
@@ -5,13 +5,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Configuration globale
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('');
 
-  // Activer CORS
-  app.enableCors();
+  // Activer CORS avec une configuration plus permissive
+  app.enableCors({
+    origin: true, // Permet toutes les origines en développement
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
   // Démarrer l'application
   await app.listen(process.env.PORT ?? 3000);
   console.log(`API Gateway is running on port ${process.env.PORT ?? 3000}`);
 }
-bootstrap();
+void bootstrap();
