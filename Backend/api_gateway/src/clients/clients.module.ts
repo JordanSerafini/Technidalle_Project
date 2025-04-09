e6a0827ed516280c45ea26c32eb24399b68e25df
JordanS <jordanserafini.74@gmail.com>
2025-04-09 16:45:03 +0200

    working

--- Backend/api_gateway/src/clients/clients.module.ts
+++ Backend/api_gateway/src/clients/clients.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  ClientsModule as NestClientsModule,
+  Transport,
+} from '@nestjs/microservices';
 import { ClientsController } from './clients.controller';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    NestClientsModule.register([
       {
         name: 'CLIENTS_SERVICE',
         transport: Transport.TCP,
