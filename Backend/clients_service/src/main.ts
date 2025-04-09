b26d90dcb348cd4a13304cd1d4b41284d47f1c8e
JordanS <jordanserafini.74@gmail.com>
2025-04-09 16:02:01 +0200

    setupt :sparkles:

--- Backend/clients_service/src/main.ts
+++ Backend/clients_service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: 'tcp',
+      options: {
+        host: '0.0.0.0',
+        port: 3002,
+      },
+    } as unknown as MicroserviceOptions,
+  );
+  await app.listen();
+  console.log('Clients service is listening on port 3002');
 }
-bootstrap();
+void bootstrap();
