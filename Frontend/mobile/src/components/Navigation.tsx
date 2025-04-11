8e305281d35b914d629f000402f184591011366e
JordanS <jordanserafini.74@gmail.com>
2025-04-11 11:52:37 +0200

    :bug: fix limit search

--- Frontend/mobile/src/components/Navigation.tsx
+++ Frontend/mobile/src/components/Navigation.tsx
@@ -4,7 +4,13 @@ export function Navigation() {
   const { currentPage, setCurrentPage } = useNavigationStore();
 
   return (
-    <view className="fixed bottom-0 left-0 right-0 bg-gray-800 flex justify-between items-center h-16">
+    <view className="fixed bottom-0 left-0 right-0 flex-col items-start justify-center">
+      {/* Search */}
+      <view className="flex flex-row justify-between items-center w-full px-4 border border-gray-700 h-10">
+      <text>ddd</text>
+      </view>
+      {/* Navigation */}
+      <view className="flex flex-row justify-between items-center w-full bg-gray-800 h-16 px-4 ">
       <text 
         className={`font-medium ${currentPage === 'dashboard' ? 'text-blue-400' : 'text-white'}`} 
         bindtap={() => setCurrentPage('dashboard')}
@@ -32,9 +38,10 @@ export function Navigation() {
       <text 
         className={`font-medium ${currentPage === 'documents' ? 'text-blue-400' : 'text-white'}`} 
         bindtap={() => setCurrentPage('documents')}
-      >
-        Documents
-      </text>
+        >
+          Documents
+        </text>
+      </view>
     </view>
   );
 } 
