769da1b7ed2b949fbe0db111db56b254f0ef15e0
JordanS <jordanserafini.74@gmail.com>
2025-04-14 10:20:05 +0200

    document service

--- Backend/documents_service/src/prisma/prisma.service.ts
+++ Backend/documents_service/src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from '@prisma/client';
 
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 @Injectable()
 export class PrismaService
   extends PrismaClient
