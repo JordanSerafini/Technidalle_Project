989cef9398e151a1b893aab2532bb9a2909426fc
JordanS <jordanserafini.74@gmail.com>
2025-04-17 11:30:52 +0200

    woprking on add client modal refacto, all ok

--- Frontend/mobile_expo/app/components/modals/clients/formulaireClient.tsx
+++ Frontend/mobile_expo/app/components/modals/clients/formulaireClient.tsx
@@ -10,61 +10,68 @@ interface ClientFormProps {
 
 export const ClientForm: React.FC<ClientFormProps> = ({ client, setClient }) => {
   return (
-    <View className="flex-1 mb-4">
+    <View className="mb-4">
+      {/* ----------------------------------------------------------------------------------------------------------------- Titre */}
       <View className="flex flex-row gap-2">
         <Ionicons name="person" size={24} color="#3b82f6" />
         <Text className="text-lg font-semibold mb-2 italic">
           Informations client
         </Text>
       </View>
-
-      <TextInput
-        className="border border-gray-300 rounded-lg p-2 mb-2"
-        placeholder="Nom de la société"
-        value={client.company_name}
-        onChangeText={(text) =>
-          setClient({ ...client, company_name: text })
-        }
-      />
-
-      <View className="flex-row space-x-2">
+      
+      <View className="flex flex-col space-y-2 gap-4">
+        {/* ----------------------------------------------------------------------------------------------------------------- Nom de la société */}
         <TextInput
-          className="flex-1 border border-gray-300 rounded-lg p-2 mb-2"
-          placeholder="Prénom"
-          value={client.firstname}
+          className="border border-gray-300 rounded-lg p-2"
+          placeholder="Nom de la société"
+          value={client.company_name}
           onChangeText={(text) =>
-            setClient({ ...client, firstname: text })
+            setClient({ ...client, company_name: text })
           }
         />
+
+        {/* ----------------------------------------------------------------------------------------------------------------- Prénom et nom */}
+        <View className="flex-row space-x-2">
+          <TextInput
+            className="flex-1 border border-gray-300 rounded-lg p-2"
+            placeholder="Prénom"
+            value={client.firstname}
+            onChangeText={(text) =>
+              setClient({ ...client, firstname: text })
+            }
+          />
+          <TextInput
+            className="flex-1 border border-gray-300 rounded-lg p-2"
+            placeholder="Nom"
+            value={client.lastname}
+            onChangeText={(text) =>
+              setClient({ ...client, lastname: text })
+            }
+          />
+        </View>
+
+        {/* ----------------------------------------------------------------------------------------------------------------- Email */}
         <TextInput
-          className="flex-1 border border-gray-300 rounded-lg p-2 mb-2"
-          placeholder="Nom"
-          value={client.lastname}
+          className="border border-gray-300 rounded-lg p-2"
+          placeholder="Email"
+          value={client.email}
           onChangeText={(text) =>
-            setClient({ ...client, lastname: text })
+            setClient({ ...client, email: text })
           }
+          keyboardType="email-address"
+        />
+
+        {/* ----------------------------------------------------------------------------------------------------------------- Téléphone */}
+        <TextInput
+          className="border border-gray-300 rounded-lg p-2"
+          placeholder="Téléphone"
+          value={client.phone}
+          onChangeText={(text) =>
+            setClient({ ...client, phone: text })
+          }
+          keyboardType="phone-pad"
         />
       </View>
-
-      <TextInput
-        className="border border-gray-300 rounded-lg p-2 mb-2"
-        placeholder="Email"
-        value={client.email}
-        onChangeText={(text) =>
-          setClient({ ...client, email: text })
-        }
-        keyboardType="email-address"
-      />
-
-      <TextInput
-        className="border border-gray-300 rounded-lg p-2 mb-2"
-        placeholder="Téléphone"
-        value={client.phone}
-        onChangeText={(text) =>
-          setClient({ ...client, phone: text })
-        }
-        keyboardType="phone-pad"
-      />
     </View>
   );
 };
