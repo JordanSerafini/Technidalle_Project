434d57a23fe7c7cdbe7f51bf32904c7c002dec2c
JordanS <jordanserafini.74@gmail.com>
2025-04-18 11:38:59 +0200

    Sync ebp > pg, pg > pg, customer ok, working on customer list frontend

--- Backend/sync_service/src/app.module.ts
+++ Backend/sync_service/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PgSyncController } from './pgToPg/pgSync.controller';
+import { PgSyncService } from './pgToPg/pgSync.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PgSyncController],
+  providers: [AppService, PgSyncService],
 })
 export class AppModule {}
