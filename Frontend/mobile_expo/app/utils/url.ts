3f17006e8dc17dcdda996e362785072ab84d427e
JordanS <jordanserafini.74@gmail.com>
2025-04-28 11:01:41 +0200

    fix document fab

--- Frontend/mobile_expo/app/utils/url.ts
+++ Frontend/mobile_expo/app/utils/url.ts
@@ -1,5 +1,5 @@
 export const url = {
-    local: 'https://4dc9-2a01-cb15-11-6800-b2a7-9ce3-5d5-e4af.ngrok-free.app/',
+    local: 'http://192.168.20.225:3000/',
     forwarded: 'https://4dc9-2a01-cb15-11-6800-b2a7-9ce3-5d5-e4af.ngrok-free.app',
 }
 
