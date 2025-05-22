8efbf335b2dabbc4cf4b76e64758ff9c70c916bd
JordanS <jordanserafini.74@gmail.com>
2025-05-22 15:00:57 +0200

    whatsapp

--- Backend/chatbot_service/src/whatsapp/whatsapp.module.ts
+++ Backend/chatbot_service/src/whatsapp/whatsapp.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WhatsappController } from './whatsapp.controller';
 import { WhatsappService } from './whatsapp.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [WhatsappController],
   providers: [WhatsappService],
 })
