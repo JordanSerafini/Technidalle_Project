5ee532612f696efc03a79d929cbe998d2265766c
JordanS <jordanserafini.74@gmail.com>
2025-05-23 14:58:05 +0200

    working

---
+++ Email_Module/email365_service/src/whatsapp/whatsapp.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { WhatsappService } from './whatsapp.service';
+import { WhatsappController } from './whatsapp.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [HttpModule, ConfigModule],
+  controllers: [WhatsappController],
+  providers: [WhatsappService],
+  exports: [WhatsappService],
+})
+export class WhatsappModule {}
