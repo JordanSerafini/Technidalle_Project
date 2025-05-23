b11b61cca78b84edf373c8f87f360422a6e9f3d2
JordanS <jordanserafini.74@gmail.com>
2025-05-23 11:03:54 +0200

    365 setup

---
+++ Email_Module/email365_service/src/utils/function.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export async function getToken() {
+  const url = `https://login.microsoftonline.com/${process.env.TENANT_ID}/oauth2/v2.0/token`;
+  const params = new URLSearchParams();
+  params.append('client_id', process.env.CLIENT_ID ?? '');
+  params.append('scope', 'https://graph.microsoft.com/.default');
+  params.append('client_secret', process.env.CLIENT_SECRET ?? '');
+  params.append('grant_type', 'client_credentials');
+
+  const res = await axios.post(url, params, {
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  });
+
+  return res.data.access_token;
+}
