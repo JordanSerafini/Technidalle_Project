533324422f0637cac4611bfd6cf28de78e78ada0
Jordan S <131240361+JordanSerafini@users.noreply.github.com>
2025-06-05 12:02:04 +0200

    feat: add basic Swagger docs

--- Backend/api_gateway/src/main.ts
+++ Backend/api_gateway/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -18,6 +19,14 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway')
+    .setDescription('Documentation des services de l\'API Gateway')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // Démarrer l'application
   await app.listen(process.env.PORT ?? 3000);
